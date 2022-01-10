9cd6a58debfe088403ba7af83b94c7c104857bdd
Carlos Nascimento <cdgodoy17@gmail.com>
2022-01-09 23:04:48 -0300

    Erro nas rotas

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import Topbar from "./components/topbar/Topbar";
 import Sidebar from "./components/sidebar/sidebar";
 import "./app.css";
 import Home from "./pages/home/Home";
+import UserList from "./pages/userList/UserList";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div>
+    <Router>
       <Topbar/>
       <div className="container">
         <Sidebar />
-        <Home/>
+
+        <Switch>
+          <Route path="/" component={Home} exact/>
+        </Switch>
+        
       </div>
-    </div>
+    </Router>
   );
 }
 
